ab5fda96c345666c6deafe823c4fa35e3048b129
Alexander Sandrock <alexander.sandrock@tu-dortmund.de>
2019-03-19 21:25:27 +0100

    Corrected error in sigma_gg, added check in tau_blr to avoid numerical error

--- photoabsorption.f90
+++ photoabsorption.f90
@@ -22,7 +22,7 @@ contains
     call dcuhre(2, 1, aa, bb, int(1e3), int(1e5), funsub, epsabs, epsrel, 0, 0, &
       res, err, l_neval, l_ier, nsub)
     tau_disk = res(1)
-    if (l_ier /= 0) tau_disk = -1.0_dp
+    !if (l_ier /= 0) tau_disk = -1.0_dp
   contains
     subroutine funsub(ndim, z, nfun, f)
       implicit none
@@ -46,6 +46,7 @@ contains
     end function l_int
 
     function R_int(log_Rt)
+      ! corrected according to Dermer et al. (2009)
       implicit none
       real(dp), intent(in) :: log_Rt
       real(dp) :: R_int, Rt, mu, st
@@ -54,7 +55,8 @@ contains
 
       mu = 1/sqrt(1 + Rt**2/l_save**2)
       st = eps0(Rt)*eps_1*(1 + z)*(1 - mu)/2
-      R_int = prefactor*phi(Rt)/(Rt**1.25_dp*(1 + Rt**2/l_save**2)**1.5_dp) &
+      R_int = prefactor*phi(Rt)**0.25_dp/(Rt**1.25_dp &
+        *(1 + Rt**2/l_save**2)**1.5_dp) &
         *sigma_gg(st)*(1 - mu)/l_save**2 &
         *Rt
     end function R_int
@@ -75,24 +77,30 @@ contains
     end function phi
   end function tau_disk
 
-  function tau_blr(eps_1, z, l_edd, rr, R_g, xi_li, R_li, eps_li, n_li)
+  function tau_blr(eps_1, z, l_edd, rr, R_g, xis_li, Rs_li, epss_li, n_li)
     implicit none
     real(dp) :: tau_blr, eps_1, z, l_edd, R_g, rr
     integer :: n_li
-    real(dp), dimension(n_li) :: xi_li, R_li, eps_li
+    real(dp), dimension(n_li) :: xis_li, Rs_li, epss_li
+    real(dp) :: xi_li, R_li, eps_li
     real(dp) :: l_save, l_err
-    integer :: l_neval, l_ier, nsub
+    integer :: l_neval, l_ier, nsub, j
     real(dp), dimension(2) :: aa, bb
-    real(dp), dimension(n_li) :: res, err
+    real(dp), dimension(1) :: res, err
 
     !call qagi(l_int, rr/R_g, 1, epsabs, epsrel, tau_blr, l_err, l_neval, l_ier)
     !call qng(l_int, 0.0_dp, R_g/rr, epsabs, epsrel, tau_blr, l_err, l_neval, l_ier)
     aa = [0.0_dp, -1.0_dp]
     bb = [R_g/rr, 1.0_dp]
-    call dcuhre(2, n_li, aa, bb, int(1e3), int(1e5), funsub, epsabs, epsrel, 0, 0, &
-      res, err, l_neval, l_ier, nsub)
-    tau_blr = sum(res)
-    if (l_ier /= 0) tau_blr = -1.0_dp
+    tau_blr = 0.0_dp
+    do j = 1, n_li
+      R_li = Rs_li(j)
+      xi_li = xis_li(j)
+      eps_li = epss_li(j)
+      call dcuhre(2, 1, aa, bb, int(1e3), int(1e5), funsub, epsabs, epsrel, 0, 0, &
+        res, err, l_neval, l_ier, nsub)
+      tau_blr = tau_blr + res(1)
+    end do
   contains
     subroutine funsub(ndim, z, nfun, f)
       implicit none
@@ -119,13 +127,17 @@ contains
 
     function mu_int(mu_re)
       implicit none
-      real(dp), dimension(n_li) :: mu_int
+      real(dp) :: mu_int
       real(dp) :: mu_re
-      real(dp), dimension(n_li) :: integ, x2, mu_star, s, Rg2_x2, aux
+      real(dp) :: integ, x2, mu_star, s, Rg2_x2, aux
 
       x2 = ((R_li/R_g)**2 + l_save**2 - 2*l_save*R_li/R_g*mu_re)
       Rg2_x2 = R_li**2 + (R_g*l_save)**2 - 2*(l_save*R_g)*R_li*mu_re
-      aux = max(0.0_dp, 1 - R_li**2/Rg2_x2*(1 - mu_re**2))
+      if (abs(mu_re) < 1) then
+        aux = max(0.0_dp, 1 - R_li**2/Rg2_x2*(1 - mu_re**2))
+      else
+        aux = 0.0_dp
+      end if
       mu_star = sqrt(aux)
       s = eps_li*eps_1*(1 + z)*(1 - mu_star)/2
       integ = 900*xi_li*l_edd/eps_li/x2*sigma_gg(s)*(1 - mu_star)
@@ -140,7 +152,7 @@ contains
     integer :: neval, ier
 
     call qagi(integrand, rr/R_g, 1, epsabs, epsrel, tau_dust, err, neval, ier)
-    if (ier /= 0) tau_dust = -1.0_dp
+    !if (ier /= 0) tau_dust = -1.0_dp
   contains
     function integrand(l)
       implicit none
@@ -159,7 +171,7 @@ contains
     real(dp) :: sigma_gg, beta
     real(dp), intent(in) :: s
 
-    if (s > 4.0_dp) then
+    if (s > 1.0_dp) then
       beta = sqrt(1 - 1/s)
       sigma_gg = 3/(8*s)*((3 - beta**4)*log((1 + beta)**2*s) &
         - 2*beta*(2 - beta**2))
